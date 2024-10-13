7e43929f6a1a223930dcdc3ad35d4123aab5579b
wafaMohamed <wafamohameddd@gmail.com>
2024-10-13 20:43:54 +0300

    view model the bussines logic in provider for toda and add the changNotifierProvider in top widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_hive/view/todo_view.dart';
+import 'package:todo_hive/view_model/todo_view_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,16 +17,19 @@ void main() async {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TodoViewModel()),
+      ],
+      child: MaterialApp(
         title: 'Todo App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: TodoListScreen(),
-      );
+      ),
+    );
   }
 }
---
+++ lib/view_model/todo_view_model.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../model/todo_model.dart';
+
+class TodoViewModel extends ChangeNotifier {
+  final Box _todoBox = Hive.box('todoBox');
+
+  // Get all Todos
+  List<Todo> get todos {
+    return _todoBox.values
+        .map((e) => Todo.fromMap(Map<String, dynamic>.from(e))).toList();
+  }
+
+  // Add a new Todo
+  void addTodo(String title) {
+    final newTodo = Todo(title: title);
+    _todoBox.add(newTodo.toMap());
+    notifyListeners();
+  }
+
+  // Update Todo
+  void updateTodo(int index, String title, bool isCompleted) {
+    final todo = Todo(title: title, isCompleted: isCompleted);
+    _todoBox.putAt(index, todo.toMap());
+    notifyListeners();
+  }
+
+  // Delete Todo
+  void deleteTodo(int index) {
+    _todoBox.deleteAt(index);
+    notifyListeners();
+  }
+
+  // Toggle the completion status of a Todo
+  void toggleTodoCompletion(int index) {
+    var todo = Todo.fromMap(Map<String, dynamic>.from(_todoBox.getAt(index)));
+    todo.isCompleted = !todo.isCompleted;
+    _todoBox.putAt(index, todo.toMap());
+    notifyListeners();
+  }
+}
