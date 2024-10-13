d87affb480b105b6734d98ce2633fd96c4088492
wafaMohamed <wafamohameddd@gmail.com>
2024-10-13 20:42:26 +0300

    setup hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_hive/view/todo_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
+  await Hive.initFlutter();
+
+  // Open the Hive box for Todos
+  await Hive.openBox('todoBox');
+
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
