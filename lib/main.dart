040df5c43b205be59731fcd4b6198eb3939cd64a
wafaMohamed <wafamohameddd@gmail.com>
2024-10-13 20:37:45 +0300

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:todo_hive/view/todo_view.dart';
-import 'package:todo_hive/view_model/todo_view_model.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Hive
-  await Hive.initFlutter();
-
-  // Open the Hive box for Todos
-  await Hive.openBox('todoBox');
-
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => TodoViewModel()),
-      ],
-      child: MaterialApp(
+    return  MaterialApp(
         title: 'Todo App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: TodoListScreen(),
-      ),
-    );
+      );
   }
 }
